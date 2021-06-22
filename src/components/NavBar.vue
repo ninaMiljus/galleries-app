e0e8e2cd184e1c3aad06bda3334e4b0e73af6eb3
ninaMiljus <nina.miljus@gmail.com>
2021-06-22 23:10:35 +0200

    Added routes, views Register and Login, store for auth, baseService and authService, authGuard.

---
+++ src/components/NavBar.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="nav">
+    <div class="nav-box">
+      <router-link to="/">Galleries</router-link> |
+    </div>
+    <div class="nav-box">
+      <input @input="search" placeholder="search" />
+    </div>
+    <div class="nav-box">
+      <template v-if="!isAuthenticated">
+        <router-link to="/register">Register</router-link> |
+        <router-link to="/login">Login</router-link>
+      </template>
+      <template v-else>
+        <a class="button" @click="logout">Logout</a>
+      </template>
+    </div>
+  </div>
+</template>
+<script>
+import { mapActions, mapGetters, mapMutations } from 'vuex';
+export default {
+  name: 'nav-bar',
+  computed: {
+    ...mapGetters('auth', ['isAuthenticated']),
+  },
+  methods: {
+    search(evt) {
+      this.setSearchTerm(evt.target.value);
+      this.getGalleries();
+    },
+    ...mapActions('galleries', ['getGalleries']),
+    ...mapMutations('galleries', ['setSearchTerm']),
+    ...mapActions('auth', ['logout']),
+  },
+};
+</script>
+<style scoped>
+.nav {
+  display: flex;
+  padding: 30px;
+}
+.nav-box {
+  flex: 1;
+}
+.nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+.nav a.router-link-exact-active {
+  color: #42b983;
+}
+.button {
+  cursor: pointer;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
+import store from './store';
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
@@ -12,5 +12,6 @@ Vue.use(IconsPlugin)
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
